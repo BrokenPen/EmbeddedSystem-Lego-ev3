349084539f40e1d4cce89bf1880778e44d18927f
Debian Live user <user@localhost.localdomain>
2017-05-10 17:12:39 +0000

    added temp code

---
+++ code/keyboard/curses_key_test.py
@@ -0,0 +1,43 @@
+#*******************************************************************************
+# Sources     : ExplainingComputers.com: Raspberry Pi Zumo Robot
+# Article     : http://explainingcomputers.com/rasp_pi_robotics.html
+# Link        : http://explainingcomputers.com/sample_code/key_robot_dance.py
+# File        : curses_key_test.py
+# access date : 2017 May 10, 16:59 UTD+8
+#******************************************************************************/
+
+#*******************************************************************************
+# Modifited : None
+#******************************************************************************/
+
+# import curses
+import curses
+
+# Get the curses window, turn off echoing of keyboard to screen, turn on
+# instant (no waiting) key response, and use special values for cursor keys
+screen = curses.initscr()
+curses.noecho() 
+curses.cbreak()
+screen.keypad(True)
+
+try:
+        while True:   
+            char = screen.getch()
+            if char == ord('q'):
+                break
+            elif char == curses.KEY_UP:
+                print "up"
+            elif char == curses.KEY_DOWN:
+                print "down"
+            elif char == curses.KEY_RIGHT:
+                print "right"
+            elif char == curses.KEY_LEFT:
+                print "left"
+            elif char == 10:
+                print "stop"    
+             
+finally:
+    #Close down curses properly, inc turn echo back on!
+    curses.nocbreak(); screen.keypad(0); curses.echo()
+    curses.endwin()
+
---
+++ code/keyboard/key_robot.py
@@ -0,0 +1,71 @@
+#*******************************************************************************
+# Sources      : 
+# Link         :
+# File         : key_robot.py
+# Title        : Use ssh to remote LEGO EV3
+# Author       : github.com/BrokenPen
+# Code version : 0.1
+# create date  : 2017 May 10, 16:59 UTD+8
+#******************************************************************************/
+
+#*******************************************************************************
+# Last Modifited : 
+#******************************************************************************/
+
+
+# import curses and GPIO
+import curses
+import RPi.GPIO as GPIO
+
+#set GPIO numbering mode and define output pins
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
+GPIO.setup(15,GPIO.OUT)
+
+# Get the curses window, turn off echoing of keyboard to screen, turn on
+# instant (no waiting) key response, and use special values for cursor keys
+screen = curses.initscr()
+curses.noecho() 
+curses.cbreak()
+screen.keypad(True)
+
+try:
+        while True:   
+            char = screen.getch()
+            if char == ord('q'):
+                break
+            elif char == curses.KEY_UP:
+                GPIO.output(7,False)
+                GPIO.output(11,True)
+                GPIO.output(13,False)
+                GPIO.output(15,True)
+            elif char == curses.KEY_DOWN:
+                GPIO.output(7,True)
+                GPIO.output(11,False)
+                GPIO.output(13,True)
+                GPIO.output(15,False)
+            elif char == curses.KEY_RIGHT:
+                GPIO.output(7,True)
+                GPIO.output(11,False)
+                GPIO.output(13,False)
+                GPIO.output(15,True)
+            elif char == curses.KEY_LEFT:
+                GPIO.output(7,False)
+                GPIO.output(11,True)
+                GPIO.output(13,True)
+                GPIO.output(15,False)
+            elif char == 10:
+                GPIO.output(7,False)
+                GPIO.output(11,False)
+                GPIO.output(13,False)
+                GPIO.output(15,False)
+             
+finally:
+    #Close down curses properly, inc turn echo back on!
+    curses.nocbreak(); screen.keypad(0); curses.echo()
+    curses.endwin()
+    GPIO.cleanup()
+    
+
